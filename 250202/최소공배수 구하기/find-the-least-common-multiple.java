dda24c9a9edfac1f47c1bd546d42e9989d19bcbf
Jiyun Kang <155218786+hasol11@users.noreply.github.com>
2025-02-02 01:41:18 +0900

    Title: 최소공배수 구하기, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250202/최소공배수 구하기/find-the-least-common-multiple.java
+++ 250202/최소공배수 구하기/find-the-least-common-multiple.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Main {
     public static void cal(int n, int m){
         int min=100;
-        for (int i=1;i<=n;i++){
-            for(int j=1;j<m;j++){
+        for (int i=1;i<=100;i++){
+            for(int j=1;j<100;j++){
                 //값이 같아야하고, 곱하기해서 나온 값중에 작아야 해 
                 if(m*j==i*n&&min>m*j){
                     min=m*j;
