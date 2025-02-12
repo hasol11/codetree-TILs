52667161823716bca74b8fd7b4bb8fd91e6f4679
Jiyun Kang <155218786+hasol11@users.noreply.github.com>
2025-02-12 10:09:27 +0900

    Title: 입력받은 수의 합과 평균, Time: 146ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250212/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.java
+++ 250212/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc =new Scanner(System.in);
         int n = sc.nextInt();
         int sum=0;
-        double avg;
+        double avg=0;
 
         for(int i=0;i<n;i++){
             int input = sc.nextInt();
